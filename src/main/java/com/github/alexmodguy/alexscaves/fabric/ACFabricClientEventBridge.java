be082f0324b0343500d0c2682bdb8c659ac3d248
CaoTrongThang <fptisthebest@gmail.com>
2026-04-21 02:26:27 +0700

    Fix the raygun doesn't show the ray trail

--- src/main/java/com/github/alexmodguy/alexscaves/fabric/ACFabricClientEventBridge.java
+++ src/main/java/com/github/alexmodguy/alexscaves/fabric/ACFabricClientEventBridge.java
@@ -1,8 +1,10 @@
 package com.github.alexmodguy.alexscaves.fabric;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import com.github.alexmodguy.alexscaves.forge_shim.common.MinecraftForge;
 import com.github.alexmodguy.alexscaves.forge_shim.event.TickEvent;
+import com.github.alexmodguy.alexscaves.forge_shim.client.event.RenderLevelStageEvent;
 
 public final class ACFabricClientEventBridge {
 
@@ -10,11 +12,13 @@ public final class ACFabricClientEventBridge {
     }
 
     public static void registerClient() {
-        ClientTickEvents.START_CLIENT_TICK.register(client ->
-            MinecraftForge.EVENT_BUS.post(new TickEvent.ClientTickEvent(TickEvent.Phase.START))
-        );
-        ClientTickEvents.END_CLIENT_TICK.register(client ->
-            MinecraftForge.EVENT_BUS.post(new TickEvent.ClientTickEvent(TickEvent.Phase.END))
-        );
+        ClientTickEvents.START_CLIENT_TICK.register(
+                client -> MinecraftForge.EVENT_BUS.post(new TickEvent.ClientTickEvent(TickEvent.Phase.START)));
+        ClientTickEvents.END_CLIENT_TICK
+                .register(client -> MinecraftForge.EVENT_BUS.post(new TickEvent.ClientTickEvent(TickEvent.Phase.END)));
+        WorldRenderEvents.AFTER_ENTITIES.register(context -> {
+            MinecraftForge.EVENT_BUS.post(new RenderLevelStageEvent(RenderLevelStageEvent.Stage.AFTER_ENTITIES,
+                    context.worldRenderer(), context.matrixStack(), 0, context.camera(), context.tickDelta()));
+        });
     }
 }
--- src/main/java/com/github/alexmodguy/alexscaves/mixin/client/LivingEntityRendererMixin.java
+++ src/main/java/com/github/alexmodguy/alexscaves/mixin/client/LivingEntityRendererMixin.java
@@ -1,7 +1,10 @@
 package com.github.alexmodguy.alexscaves.mixin.client;
 
 import com.github.alexmodguy.alexscaves.client.render.entity.LivingEntityRendererAccessor;
+import com.github.alexmodguy.alexscaves.forge_shim.client.event.RenderLivingEvent;
+import com.github.alexmodguy.alexscaves.forge_shim.common.MinecraftForge;
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.LivingEntityRenderer;
@@ -9,15 +12,37 @@ import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntityRenderer.class)
 public abstract class LivingEntityRendererMixin extends EntityRenderer implements LivingEntityRendererAccessor {
 
-    @Shadow protected abstract void scale(LivingEntity living, PoseStack poseStack, float f);
-    @Shadow protected abstract boolean addLayer(RenderLayer<?, ?> renderLayer);
+    @Shadow
+    protected abstract void scale(LivingEntity living, PoseStack poseStack, float f);
+
+    @Shadow
+    protected abstract boolean addLayer(RenderLayer<?, ?> renderLayer);
 
     protected LivingEntityRendererMixin(EntityRendererProvider.Context context) {
         super(context);
+    }
+
+    @Inject(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At("HEAD"), cancellable = true)
+    private void ac_beforeRender(LivingEntity entity, float f1, float f2, PoseStack poseStack,
+            MultiBufferSource bufferSource, int i, CallbackInfo ci) {
+        if (MinecraftForge.EVENT_BUS.post(new RenderLivingEvent.Pre(entity, (LivingEntityRenderer) (Object) this, f2,
+                poseStack, bufferSource, i))) {
+            ci.cancel();
+        }
+    }
+
+    @Inject(method = "render(Lnet/minecraft/world/entity/LivingEntity;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V", at = @At("TAIL"))
+    private void ac_afterRender(LivingEntity entity, float f1, float f2, PoseStack poseStack,
+            MultiBufferSource bufferSource, int i, CallbackInfo ci) {
+        MinecraftForge.EVENT_BUS.post(new RenderLivingEvent.Post(entity, (LivingEntityRenderer) (Object) this, f2,
+                poseStack, bufferSource, i));
     }
 
     @Override
