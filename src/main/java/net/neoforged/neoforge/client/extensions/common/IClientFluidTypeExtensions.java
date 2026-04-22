106cfda2d699bd766ec28a4f4f30b67db6279680
CaoTrongThang <fptisthebest@gmail.com>
2026-04-22 16:28:06 +0700

    Readded Neoforge :), god I'm bad

---
+++ src/main/java/net/neoforged/neoforge/client/extensions/common/IClientFluidTypeExtensions.java
@@ -0,0 +1,19 @@
+package net.neoforged.neoforge.client.extensions.common;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+
+public interface IClientFluidTypeExtensions {
+
+    default ResourceLocation getStillTexture() {
+        return null;
+    }
+
+    default ResourceLocation getFlowingTexture() {
+        return null;
+    }
+
+    default ResourceLocation getRenderOverlayTexture(Minecraft minecraft) {
+        return null;
+    }
+}
